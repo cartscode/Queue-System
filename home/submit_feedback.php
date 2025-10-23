011512ba3b28b3ce0507dda693f1a750e7147686
africaacecorpuz-create <africa.ace.corpuz@gmail.com>
2025-10-23 21:05:42 +0800

    Add files via upload

---
+++ home/submit_feedback.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO feedback (name, email, message) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $name, $email, $message);
+
+if ($stmt->execute()) {
+  header("Location: index.html");
+  exit();
+} else {
+  echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
